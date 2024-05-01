e4108087caa8929d3a64a9bdfc64085cf6c93f4c
Ian Perfitt <iancp@protonnmail.com>
2024-05-01 12:49:41 -0500

    practiced basic junit test annotations

---
+++ src/test/java/org/practice/JUnit5Tests.java
@@ -0,0 +1,41 @@
+package org.practice;
+
+import org.junit.jupiter.api.*;
+
+public class JUnit5Tests {
+
+    @BeforeAll
+    static void setup() {
+        System.out.println("@BeforeAll executed");
+    }
+
+    @BeforeEach
+    void setupThis() {
+        System.out.println("@BeforeEach executed");
+    }
+
+    @Tag("DEV")
+    @Test
+    void testCalcOne() {
+        System.out.println("======TEST ONE EXECUTED=======");
+        Assertions.assertEquals(4, Math.addExact(2, 2));
+    }
+
+    @Tag("PROD")
+    @Disabled
+    @Test
+    void testCalcTwo() {
+        System.out.println("======TEST TWO EXECUTED=======");
+        Assertions.assertEquals(6, Math.addExact(3, 3));
+    }
+
+    @AfterEach
+    void tearThis() {
+        System.out.println("@AfterEach executed");
+    }
+
+    @AfterAll
+    static void tear() {
+        System.out.println("@AfterAll executed");
+    }
+}
